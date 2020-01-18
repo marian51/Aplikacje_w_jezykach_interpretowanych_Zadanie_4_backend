20ffe07a6f9bffae1c24f810849e02a68e7b8f32
Mariusz <mariusz.jozwiak@int.pl>
2020-01-18 18:52:24 +0100

    Wyszukiwanie pojedynczej kategorii

--- app/controllers/category.controller.js
+++ app/controllers/category.controller.js
@@ -45,3 +45,18 @@ exports.findAll = (req, res) => {
             });
         });
  };
+
+ exports.findOne = (req, res) => {
+     const id = req.params.id;
+
+     Category.findByPk(id)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Nieokreślony błąd 500 przy pobieraniu kategorii."
+            });
+        });
+ };
