2014a03e388a5cf5af47801b213ab25e9666b458
Mariusz <mariusz.jozwiak@int.pl>
2020-01-20 01:00:45 +0100

    Tworzenie obiektu zamówienia i zapis w bazie

--- app/controllers/order.controller.js
+++ app/controllers/order.controller.js
@@ -1,6 +1,31 @@
-/*const db = require("../models");
+const db = require("../models");
 const Order = db.orders;
 const Op = db.Sequelize.Op;
 
-exports.create */
-// TODO tu skończyłem
+exports.create = (req, res) => {
+    if(!req.body.totalPrice) {
+        res.status(400).send({
+            message: "Zamówienie nie ma wartości!"
+        });
+        return;
+    }
+
+    const order = {
+        userName: req.body.userName,
+        userEmail: req.body.userEmail,
+        userPhone: req.body.userPhone,
+        stateId: req.body.stateId,
+        totalPrice: req.body.totalPrice
+    };
+
+    Order.create(order)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Błąd"
+            })
+        });
+};
